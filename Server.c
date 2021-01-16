e5dfbb688809e8edfccc53e70ee4864866f73ba3
Elay <illydahan9@gmail.com>
2021-01-16 16:15:11 +0200

    Non blocking read operations - socket now run smoothly. TODO: add warning message and improve error handling.

--- Server.c
+++ Server.c
@@ -1,6 +1,7 @@
 #include "Server.h"
 #define BUF_SIZE 516 // Maximum message size
-
+#define TIME_OUT 3
+#define MAX_ERRORS 7
 
 
 const int WAIT_FOR_PACKET_TIMEOUT = 3;
@@ -42,6 +43,11 @@ int main(int argc, char *argv[])
     unsigned int client_len = sizeof(cli_addr);
     // At this point we are ready for communication.
 
+    // Maximum timegap that no packet can be recieved
+    struct timeval timeout;
+
+    // File descriptor set. only Server FD interest's us.
+    fd_set rfds;
     
 
     // Main server loop
@@ -49,33 +55,63 @@ int main(int argc, char *argv[])
     {
 
         printf("Waiting on port %d \n", htons(server_addr.sin_port));
-        uint32_t block_number = 0;
-        int is_active = 0;
+        
+        int is_active = 1;
         // The file we recieved
         FILE *in_file = NULL;
-
         // here we will store the initial write request.
         struct WRQ *wrq_packet;
         struct DATA *data_packet;
         char *ack;
-        uint32_t recvlen, writelen;
-        unsigned char op_code[2];
-        recvlen = recvfrom(sockfd, &op_code, 2, MSG_WAITALL,(struct sockaddr *)&cli_addr, (unsigned int *)&client_len);
+        int msg_recvlen, writelen;
         
-        is_active = recvlen > 0 ? 1 : 0;
+        //recvlen = recvfrom(sockfd, &op_code, 2, MSG_WAITALL,(struct sockaddr *)&cli_addr, (unsigned int *)&client_len);
+        uint32_t block_number = 0;
+        int timeout_counter = 0;
+
         
+
+
+
         while(is_active) 
         {
-            recvlen = recvfrom(sockfd, &op_code, 2, MSG_WAITALL,(struct sockaddr *)&cli_addr, (unsigned int *)&client_len);
-            if(recvlen > 0) 
+            timeout.tv_sec = 3;
+            timeout.tv_usec = 0;
+            FD_ZERO(&rfds);
+            FD_SET(sockfd, &rfds);
+            int ret = select(sockfd+1, &rfds, NULL, NULL, &timeout);
+            if (ret == -1) {
+                // error 
+                printf("Error");    
+                break;
+                
+            }
+            else if(ret == 0) {
+                // timeout
+                printf("Time out\n");
+                ++timeout_counter;
+                if (timeout_counter >= WAIT_FOR_PACKET_TIMEOUT)
+                    break;
+            }
+            else {
+                // recieve the opcode
+                msg_recvlen = recvfrom(sockfd, (char *)recv_buf, BUF_SIZE, 0, (struct sockaddr *)&cli_addr, (unsigned int *)&client_len);
+                if (msg_recvlen <= 0) {
+                    // network error
+                    printf("error, %s\n", strerror(errno));
+                    printf("debug::net error\n");
+                }
+
+            }
+            //recvlen = recvfrom(sockfd, &op_code, 2, MSG_WAITALL,(struct sockaddr *)&cli_addr, (unsigned int *)&client_len);
+            if(msg_recvlen > 0) 
             {
-                int parsed_opcode = op_code[1];
+                int parsed_opcode = recv_buf[1];
                 switch (parsed_opcode)
                 {
                 case OP_WRQ:
                     printf("got WRQ..\n");
                     // TODO: validate first and only wrq of the session.
-                    recvlen = recvfrom(sockfd, (char *)recv_buf, BUF_SIZE - 2, MSG_WAITALL,(struct sockaddr *)&cli_addr, (unsigned int *)&client_len);
 
                     wrq_packet = parse_WRQ_packet(recv_buf);
                     in_file = fopen(wrq_packet->file_name, "w");
@@ -83,9 +119,13 @@ int main(int argc, char *argv[])
                     free(wrq_packet);
 
                     ack = make_ACK_packet(block_number);
-
-                    writelen = sendto(sockfd, (const void *)ack, sizeof(struct ACK), 0, (struct sockaddr *)&cli_addr, client_len);
-
+                    writelen = 0;
+                    while (writelen <= 0)
+                    {
+                        writelen = sendto(sockfd, (const void *)ack, sizeof(struct ACK), 0, (struct sockaddr *)&cli_addr, client_len);
+                        printf("error, %s\n", strerror(errno));
+                    }
+                    
                     free(ack);
                     block_number++;
                     break;
@@ -94,7 +134,7 @@ int main(int argc, char *argv[])
                     break;
 
                 case OP_DATA:
-                    if(in_file == NULL) {
+                    if(!in_file) {
                         // error. cant get data before write request
                         printf("error. cant get data before write request\n");
                         fclose(in_file);
@@ -102,17 +142,24 @@ int main(int argc, char *argv[])
 
                     }
                     printf("got data request\n");
-                    recvlen = recvfrom(sockfd, (char *)recv_buf, BUF_SIZE - 2, MSG_WAITALL,(struct sockaddr *)&cli_addr, &client_len);
-                    if(recvlen < BUF_SIZE - 2) {
+                    // TODO: validate first and only wrq of the session.
+                                        
+                    //recvlen = recvfrom(sockfd, (char *)recv_buf, BUF_SIZE - 2, MSG_WAITALL,(struct sockaddr *)&cli_addr, &client_len);
+                    if(msg_recvlen < BUF_SIZE) {
                         // last data packet recieved, send ack and end session.
+                        printf("DEBUG:: last packet..\n");
                         is_active = 0;
                     }
-                    data_packet = parse_DATA_packet(recv_buf, recvlen - 4);
-                    fwrite(data_packet->data_buffer, sizeof(char), recvlen - 4, in_file);
+                    data_packet = parse_DATA_packet(recv_buf, msg_recvlen - 4);
+                    fwrite(data_packet->data_buffer, sizeof(char), msg_recvlen - 4, in_file);
                     free(data_packet);
 
                     ack = make_ACK_packet(block_number);
-                    writelen = sendto(sockfd, (const void *)ack, sizeof(struct ACK), 0, (struct sockaddr *)&cli_addr, client_len);
+                    writelen = 0;
+                    while (writelen <= 0)
+                    {
+                        writelen = sendto(sockfd, (const void *)ack, sizeof(struct ACK), 0, (struct sockaddr *)&cli_addr, client_len);
+                    }
                     free(ack);
 
                     block_number++;
@@ -124,9 +171,11 @@ int main(int argc, char *argv[])
                 }
                 writelen = 0;
             }
+            msg_recvlen = 0;
             writelen++;
         }    
-        fclose(in_file);
+        if(in_file)
+            fclose(in_file);
     }
     return 0;
 }
--- Server.h
+++ Server.h
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h> 
 #include <fcntl.h> 
+#include <errno.h>
 
 #include <sys/socket.h>
-
+#include <sys/select.h>
 
 #include "tftpTools.h"
 
